ec6e14bee27cdad0c248cc316c77dd7066609c37
Cyanase <108136209+CyanaseInc@users.noreply.github.com>
2025-07-02 08:53:52 +0300

    Create api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { API_BASE_URL } from './urls';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor
+api.interceptors.request.use(
+  (config) => {
+    // You can add authentication tokens or other headers here
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+  
+      console.error('API Error:', error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('No response received:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
